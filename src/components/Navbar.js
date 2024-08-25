313a490ac343401ddadc84161af5246e8aebcee8
dattaAwchar <dattaawchar13@gmail.com>
2024-08-25 11:04:12 +0530

    updated navbar to fixed position and corrected bugs in news.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,9 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <nav className="navbar fixed-top navbar-expand-lg bg-primary" data-bs-theme="dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/newsMonkey">NewsMonkey</Link>
+                <Link className="navbar-brand" to="/">NewsMonkey</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroll-component';
-
 
 export class News extends Component {
 
@@ -12,6 +11,7 @@ export class News extends Component {
         pageSize: 3,
         category: 'general',
     }
+
     static propTypes = {
         topic: PropTypes.string,
         pageSize: PropTypes.number,
@@ -34,50 +34,34 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(20);
 
-        this.props.setProgress(20)        
-
-        // API Key 1 of codermythos@gmail.com
-        const url = `https://newsapi.org/v2/top-headlines?q=${this.props.topic}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;        
+        const url = `https://newsapi.org/v2/top-headlines?q=${this.props.topic}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
 
         this.setState({ loadingGif: true });
-        this.props.setProgress(40)
+        this.props.setProgress(40);
 
         let data = await fetch(url);
-        this.props.setProgress(60)
+        this.props.setProgress(60);
         let parsedData = await data.json();
         console.log(parsedData); // Check the API response here
-        this.props.setProgress(80)
+        this.props.setProgress(80);
         this.setState({
             articles: parsedData.articles || [], // Fallback to an empty array if no articles
             totalResults: parsedData.totalResults,
             loadingGif: false,
         });
-        this.props.setProgress(100)        
-
+        this.props.setProgress(100);
     }
 
     async componentDidMount() {
-        this.updateNews()
+        this.updateNews();
     }
 
-    // Previous Button
-    // handlePreviousClick() {
-    //     this.setState({ page: this.state.page - 1 });
-    //     this.updateNews()
-    // }
-
-    // Next Button
-    // handleNextClick() {
-    //     this.setState({ page: this.state.page + 1 });
-    //     this.updateNews()
-    // }
-
     fetchMoreData = async () => {
-        this.setState({ page: this.state.page + 1 })
+        this.setState({ page: this.state.page + 1 });
 
-        // API Key 1 of codermythos@gmail.com
-        const url = `https://newsapi.org/v2/top-headlines?q=${this.props.topic}&apiKey=${this.props.apiKey}=${this.state.page}&pageSize=${this.props.pageSize}`;
+        const url = `https://newsapi.org/v2/top-headlines?q=${this.props.topic}&apiKey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
 
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -89,22 +73,22 @@ export class News extends Component {
         });
     }
 
-
-
     render() {
         return (
-            // <div className='container my-2'>
             <>
-                <h1 className="text-center my-3">NewsMonkey - Top {this.capitalizeFirstLetter(this.props.topic)} Headlines</h1>
+                <h1 className="text-center" style={{marginTop:'71px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.topic)} Headlines</h1>
 
                 {this.state.loadingGif && <Spinner />}
 
-                <InfiniteScroll dataLength={this.state.articles.length} next={this.fetchMoreData} hasMore={this.state.articles.length !== this.state.totalResults} loader={<Spinner />} >
-
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length < this.state.totalResults}
+                    loader={<Spinner />}
+                >
                     <div className="container">
-
                         <div className="row">
-                            {this.state.articles && this.state.articles.length > 0 ? (this.state.articles.map((element) => (
+                            {this.state.articles.map((element) => (
                                 <div className="col-md-3 my-3 mx-5" key={element.url}>
                                     <NewsItem
                                         title={element.title ? element.title : ""}
@@ -115,23 +99,13 @@ export class News extends Component {
                                         newsUrl={element.url}
                                     />
                                 </div>
-                            ))
-                            ) : (
-                                !this.state.loadingGif && <p className="text-center">No articles available.</p>
-                            )}
+                            ))}
                         </div>
                     </div>
                 </InfiniteScroll>
-
-
-                {/* <div className="container d-inline gap-2 col-2 mx-auto my-2">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-primary mx-2 my-2" style={{ width: '110px' }} onClick={this.handlePreviousClick}> &#x2190; Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-primary mx-2 my-2" style={{ width: '110px' }} onClick={this.handleNextClick}>Next &#x2192;</button>
-                </div> */}
             </>
         );
     }
-
 }
 
 export default News;
